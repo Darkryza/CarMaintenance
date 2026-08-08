851e4439cf4f1014e2646bb74588e9f30393adcb
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-08-08 20:14:25 +0800

    Refactor Navbar component layout and styling for improved design consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 const Navbar = () => {
   return (
-    <div className="w-[30%] border-r border-slate-800 bg-slate-900 p-3 flex flex-col">
+    <div className=" border-r border-slate-800 bg-slate-900 p-5 pr-15 flex flex-col">
       {/* logo and title */}
-      <div className="flex gap-3">
-        <div className="p-2 bg-purple-500 flex justify-center items-center rounded-sm">
-          C
+      <div className="flex gap-3 items-center">
+        <div className="w-5 h-5 p-3 bg-purple-500 flex justify-center items-center rounded-sm">
+          <span className="text-black font-bold">C</span>
         </div>
+        <h1 className="font-bold">CarMaint</h1>
       </div>
     </div>
   );
